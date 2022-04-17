3d1891c7e8abbcdc73af74de14a109607c7d8af3
Fan2Programmation <thibaultterrie1@gmail.com>
2022-04-17 23:36:49 +0200

    on repart de 0 et ça fonctionne

--- clean_slate.py
+++ clean_slate.py
@@ -4,8 +4,18 @@ import config
 
 bot = commands.Bot(command_prefix='?')
 
+@bot.event
+async def on_ready():
+    print('VENGABOT armé')
+
 @bot.command(name='test')
 async def essai(ctx):
     await ctx.channel.send('bravo pour cet essai fructueux')
+
+@bot.event
+async def on_message(message):
+    text = message.content.lower()
+    if 'vengabot' in text:
+        await message.channel.send('ftg')
 
 bot.run(config.key())
