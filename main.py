3d420fef9c8fb94b8a0ce7335eaf08c13a0fb6d9
Malo-LC <malolc2001@gmail.com>
2022-04-17 02:42:08 +0200

    voila

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import discord
 import random
+import config
 
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print("VENGABOT prêt à faire des dingueries")
 
+
 @client.event
 async def on_message(message):
-    phrases = ["oh le zinc ca va en maude robot ou quoi", "keskia couzin pk tu m @"]
+    phrases = ["oh le zinc ca va en maude robot ou quoi",
+               "keskia couzin pk tu m @"]
     if "vengabot" in message.content.lower():
         await message.channel.send("".join(random.sample(phrases, 1)))
 
-client.run("TOKEN SECRET")
+
+client.run(config.key())
