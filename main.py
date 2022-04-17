b594a8b3997c0f658b15b0fcbc943c3fc7586ff2
Malo-LC <malolc2001@gmail.com>
2022-04-17 21:13:49 +0200

    !vdel 50000000

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from discord.ext import commands
 import random
 import config
+import discord
 
-vengabot = commands.Bot(command_prefix="v")
+
+default_intents = discord.Intents.default()
+# Vous devez activer les intents dans les paramètres du Bot
+default_intents.members = True
+vengabot = commands.Bot(command_prefix="!v", intents=default_intents)
+
+# jure ca marche la??eeeeee LESSSSGOOOOOOOOOOOO
+
+#  grosse dindigzgnuerie mon poto go coder main dans la main
+
+# y'a un chat normalement regarde ou il est --- sesssion chat
+
+# j'installe visual studio jsuis sur le navigateur ya pas de chat = azi
 
 
 @vengabot.event
@@ -10,10 +23,9 @@ async def on_ready():
     print("VENGABOT prêt à faire des dingueries")
 
 
-@vengabot.command(name='Del')
+@vengabot.command(name="del")
 async def delete(context, num: int):
     messages = await context.channel.history(limit=num + 1).flatten()
-
     for each in messages:
         await each.delete()
 
@@ -25,12 +37,9 @@ async def on_message(message):
     if "vengabot" in message.content.lower():
         await message.channel.send("".join(random.sample(phrases, 1)))
 
-
-@vengabot.event
-async def on_typing(channel, user, when):
-    nom = str(user)[:-5]
-    await channel.send("arrete d'écrire mon reuf " + nom)
+# @vengabot.event
+# async def on_typing(channel, user, when):
+#     nom = str(user)[:-5]
+#     await channel.send("arrete d'écrire mon reuf " + nom)
 
 vengabot.run(config.key())
-
-#test pour tout repush
