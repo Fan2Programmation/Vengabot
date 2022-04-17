8053ba1d35086991d16c98f78258a0c0b2cf1b8b
Malo-LC <malolc2001@gmail.com>
2022-04-17 21:43:06 +0200

    saoulé

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 import numpy
-import discord
 from discord.ext import commands
 import random
 import config
+import discord
 
 
 default_intents = discord.Intents.default()
-# Vous devez activer les intents dans les paramètres du Bot
 default_intents.members = True
 vengabot = commands.Bot(command_prefix="!v", intents=default_intents)
-
-# jure ca marche la??eeeeee LESSSSGOOOOOOOOOOOO
-
-#  grosse dindigzgnuerie mon poto go coder main dans la main
-
-# y'a un chat normalement regarde ou il est --- sesssion chat
-
-# j'installe visual studio jsuis sur le navigateur ya pas de chat = azi
 
 
 @vengabot.event
@@ -26,9 +17,15 @@ async def on_ready():
 
 @vengabot.command(name="del")
 async def delete(context, num: int):
+    print("oui")
     messages = await context.channel.history(limit=num + 1).flatten()
     for each in messages:
         await each.delete()
+
+
+@vengabot.command(name="test")
+async def test():
+    print('camarche')
 
 
 @vengabot.event
@@ -37,6 +34,7 @@ async def on_message(message):
                "keskia couzin pk tu m @"]
     if "vengabot" in message.content.lower():
         await message.channel.send("".join(random.sample(phrases, 1)))
+
 
 # @vengabot.event
 # async def on_typing(channel, user, when):
