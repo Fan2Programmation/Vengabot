06220eab196e153817dcf4a9357c85641b2b731d
Fan2Programmation <103863802+Fan2Programmation@users.noreply.github.com>
2022-04-18 19:32:45 +0200

    Update bugtest.py
    La fonction en ##### fait bug toutes les fonctions en gros

--- bugtest.py
+++ bugtest.py
@@ -1,11 +1,22 @@
 import discord
 from discord.ext import commands
+import random
+import config
 
 bot = commands.Bot(command_prefix='?')
 
 @bot.event
 async def on_ready():
     print("bot prêt")
+
+# @bot.event
+# async def on_message(message):
+#     insultes = [
+#         "keskia couzin pk tu m'@",
+#         "oh le zinc en maude robot ou quoi"
+#     ]
+#     if 'vengabot' in message.content.lower():
+#         await message.channel.send(random.choice(insultes))
 
 @bot.command(name='1')
 async def premier(ctx):
@@ -20,5 +31,7 @@ async def troisieme(ctx):
     print("troisieme passe")
 
 @bot.command(name='4')
-async def quatrieme(ctx):
+async def troisieme(ctx):
     print("quatrieme passe")
+
+bot.run(config.key())
