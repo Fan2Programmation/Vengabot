e16832f7c7274912950f5236809ff351e4db000f
Fan2Programmation <103863802+Fan2Programmation@users.noreply.github.com>
2022-04-18 16:09:08 +0000

    Tout neuf, ça taff sur le pet

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import config
+
+bot = commands.Bot(command_prefix='?')
+
+@bot.event
+async def on_ready():
+    print("c'est bon panique pas")
+
+@bot.command(name='re')
+async def prog(ctx):
+    await ctx.channel.send('on réessaie')
+
+@bot.command(name='pet')
+async def fart(ctx):
+    if ctx.author.voice is None:
+        await ctx.channel.send("T'es pas dans un vocal mon con")
+    else:
+        await ctx.author.voice.channel.connect()
+
+bot.run(config.key())
