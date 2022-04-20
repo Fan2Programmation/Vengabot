adde893edff7a99a46f71f552b92229dace81231
Malo-LC <malolc2001@gmail.com>
2022-04-20 18:45:02 +0200

    re

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from discord.ext import commands
 import random
 import config
 from insulte import *
+import youtube_dl
+import os
+from discord import FFmpegPCMAudio
 
+import yt_dlp
 
 vengabot = commands.Bot(command_prefix='?')
 
@@ -43,6 +47,39 @@ async def delete(ctx, num: int):
     messages = await ctx.channel.history(limit=num + 1).flatten()
     for each in messages:
         await each.delete()
+
+
+@vengabot.command(name='musicYT')
+async def musicYT(ctx, url: str):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        voice = await channel.connect()
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        for file in os.listdir("./"):
+            if file.endswith(".mp3"):
+                os.rename(file, "song.mp3")
+        source = FFmpegPCMAudio("song.mp3")
+        player = voice.play(source)
+
+    else:
+        await ctx.channel.send("T'es pas dans un vocal mon con")
+
+
+# @vengabot.command(name='stop')
+# async def stop(ctx):
+#     test = ctx.bot.is_connected
+#     print(test)
 
 
 @vengabot.command(name='insulte')
